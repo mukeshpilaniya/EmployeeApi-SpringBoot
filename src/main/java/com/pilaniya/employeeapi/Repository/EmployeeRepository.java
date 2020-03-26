6d59e00654649d2aa5fbbc044c03dc28f2642bdc
pilaniya <pilaniyamukesh0@gmail.com>
2020-03-27 00:35:33 +0530

    cross origin

--- src/main/java/com/pilaniya/employeeapi/Repository/EmployeeRepository.java
+++ src/main/java/com/pilaniya/employeeapi/Repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.pilaniya.employeeapi.Repository;
 
 import com.pilaniya.employeeapi.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*")
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 }
